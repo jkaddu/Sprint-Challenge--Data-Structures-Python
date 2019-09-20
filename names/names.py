f65224e2c67774a8a46b0b460808db82dc91042e
jkaddu <jkaddu@yahoo.com>
2019-09-20 10:39:01 -0700

    finished BST

--- names/names.py
+++ names/names.py
@@ -6,7 +6,51 @@ class BinarySearchTree:
         self.left = left
         self.right = right
 
-        
+    def insert(self.value):
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, value): 
+        if self.value == target:
+            return True
+
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
+    def get_max(self):
+        if not self:
+            return False
+        else:
+            if not self.right:
+                return self.value
+            else:
+                return self.right.get_max()
+
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
